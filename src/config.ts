8bf00cffac718c916e31c3c38c0b8a6b0f9beb00
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-06-13 10:45:01 -0600

    feat: agregar variable para host de base de datos

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@
 
 // Credenciales de la base de datos
 export const POSTGRES_USER = process.env.POSTGRES_USER
+export const POSTGRES_HOST = process.env.POSTGRES_HOST
 export const POSTGRES_DB = process.env.POSTGRES_DB
 export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
 
--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,15 @@
 import pg from 'pg'
-import { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER } from './config.js'
+import {
+  POSTGRES_DB,
+  POSTGRES_PASSWORD,
+  POSTGRES_USER,
+  POSTGRES_HOST,
+} from './config.js'
 
 // Conexión a la base de datos
 export const pool = new pg.Pool({
   user: POSTGRES_USER,
-  host: 'localhost', // Todo: Agregar variable de entorno para el host
+  host: POSTGRES_HOST || 'localhost',
   database: POSTGRES_DB,
   password: POSTGRES_PASSWORD,
   port: 5432,
